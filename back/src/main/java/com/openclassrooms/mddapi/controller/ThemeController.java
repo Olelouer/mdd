f41a9db67cfea3742e39fd389f4b1f72e663e8dd
Olelouer <olivier.lelouer@protonmail.com>
2025-05-02 11:15:58 +0200

    add subscribe + unsubscribe requests

--- back/src/main/java/com/openclassrooms/mddapi/controller/ThemeController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/ThemeController.java
@@ -1,12 +1,13 @@
 package com.openclassrooms.mddapi.controller;
 
+import com.openclassrooms.mddapi.dto.GlobalMessageResponse;
 import com.openclassrooms.mddapi.dto.ThemeListResponse;
+import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.service.ThemeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST Controller handling themes operations (CRUD).
@@ -25,5 +26,37 @@ public class ThemeController {
     @GetMapping
     public ResponseEntity<ThemeListResponse> getAllThemes() {
         return ResponseEntity.ok(themeService.getAllThemes());
+    }
+
+    /**
+     * ASubscribes the authenticated user to a specific theme.
+     *
+     * @param themeId Theme ID
+     * @param currentUser Authenticated user object
+     * @return Success message
+     */
+    @PostMapping("/{themeId}/subscribe")
+    public ResponseEntity<GlobalMessageResponse> subscribeToTheme(
+            @PathVariable Long themeId,
+            @AuthenticationPrincipal User currentUser
+    ) {
+        String userEmail = currentUser.getEmail();
+        return ResponseEntity.ok(themeService.subscribeUserToTheme(userEmail, themeId));
+    }
+
+    /**
+     * Handles HTTP request to unsubscribe authenticated user to theme
+     *
+     * @param themeId Theme ID
+     * @param currentUser Authenticated user object
+     * @return Success message
+     */
+    @DeleteMapping("/{themeId}/unsubscribe")
+    public ResponseEntity<GlobalMessageResponse> unsubscribeFromTheme(
+            @PathVariable Long themeId,
+            @AuthenticationPrincipal User currentUser
+    ) {
+        String userEmail = currentUser.getEmail();
+        return ResponseEntity.ok(themeService.unsubscribeUserFromTheme(userEmail, themeId));
     }
 }
--- back/src/main/java/com/openclassrooms/mddapi/service/ThemeService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/ThemeService.java
@@ -1,16 +1,24 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.dto.GlobalMessageResponse;
 import com.openclassrooms.mddapi.dto.ThemeListResponse;
+import com.openclassrooms.mddapi.model.Theme;
+import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.repository.ThemeRepository;
 import com.openclassrooms.mddapi.mapper.ThemeMapper;
+import com.openclassrooms.mddapi.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @Service
 @RequiredArgsConstructor
 public class ThemeService {
     private final ThemeRepository themeRepository;
     private final ThemeMapper themeMapper;
+    private final UserRepository userRepository;
 
     /**
      * Retrieves a list of all available themes.
@@ -19,5 +27,61 @@ public class ThemeService {
      */
     public ThemeListResponse getAllThemes() {
         return new ThemeListResponse(themeMapper.toResponseList(themeRepository.findAll()));
+    }
+
+    /**
+     * Subscribes a user identified by email to a specific theme.
+     *
+     * @param userEmail User's email address.
+     * @param themeId Theme ID
+     * @return Message if subscription successful.
+     * @throws EntityNotFoundException if the user or theme is not found.
+     * @throws IllegalArgumentException if the user is already a subscriber.
+     */
+    @Transactional
+    public GlobalMessageResponse subscribeUserToTheme(String userEmail, Long themeId) {
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with email: " + userEmail));
+
+        Theme theme = themeRepository.findById(themeId)
+                .orElseThrow(() -> new EntityNotFoundException("Theme not found with ID: " + themeId));
+
+        if (user.getSubscribedThemes().contains(theme)) {
+            throw new IllegalArgumentException("The user is already subscribed to this theme.");
+        }
+
+        user.getSubscribedThemes().add(theme);
+
+        userRepository.save(user);
+
+        return new GlobalMessageResponse("Successful theme subscription " + themeId);
+    }
+
+    /**
+     * Unsubscribes authenticated user from a theme
+     *
+     * @param userEmail User's email address.
+     * @param themeId Theme ID
+     * @return Message if unsubscription successful.
+     * @throws EntityNotFoundException if the user or theme is not found.
+     * @throws IllegalArgumentException if the user was not subscribed to this theme.
+     */
+    @Transactional
+    public GlobalMessageResponse unsubscribeUserFromTheme(String userEmail, Long themeId) {
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with email : " + userEmail));
+
+        Theme theme = themeRepository.findById(themeId)
+                .orElseThrow(() -> new EntityNotFoundException("Theme not found with ID : " + themeId));
+
+        if (!user.getSubscribedThemes().contains(theme)) {
+            throw new IllegalArgumentException("The user is not subscribed to this theme");
+        }
+
+        user.getSubscribedThemes().remove(theme);
+
+        userRepository.save(user);
+
+        return new GlobalMessageResponse("Successful unsubscription to theme " + themeId);
     }
 }
