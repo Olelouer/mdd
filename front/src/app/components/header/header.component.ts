feee17a7d33b5413f4ec53c9c5b3cbc2f6a5edaa
Olelouer <olivier.lelouer@protonmail.com>
2025-05-16 17:52:29 +0200

    create mobile panel + feed adjustments

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cpn-header',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
+  isPanelOpen: boolean = false;
 
   get userLogged(): boolean {
     return this.authService.isLoggedIn();
@@ -26,6 +28,14 @@ export class HeaderComponent {
     } else {
       this.router.navigate([""]);
     }
+  }
+
+  openPanel(): void {
+    this.isPanelOpen = true;
+  }
+
+  closePanel(): void {
+    this.isPanelOpen = false;
   }
 
 } 
